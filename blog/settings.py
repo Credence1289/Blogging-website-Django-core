78e4f1701f0d0fb042859e52e93343cab3dc9f79
Vinayak Dewoolkar <dewoolkar.vinayak@gmail.com>
2026-05-02 01:13:09 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
