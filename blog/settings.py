63f5437be177e6175a2774be9828e90f81af0938
Vinayak Dewoolkar <dewoolkar.vinayak@gmail.com>
2026-05-02 02:08:00 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.postgresql',
+        'DATABASE_URL' : env('DATABASE_URL'),
         'NAME' : env('DB_NAME'),
         'USER' : env('DB_USER'),
         'PASSWORD' : env('DB_PASSWORD'),
